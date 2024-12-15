5f39f1733170e557bf1b8229903694ba4d22ed62
IT21161674 <it21161674@my.sliit.lk>
2024-12-15 17:08:23 +0530

    Added greatings in sinhala and tamil languages

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -2,13 +2,6 @@ import React, { useState } from "react";
 import '../styles/WelcomePage.css';
 
 const WelcomePage = () => {
-  const [language, setLanguage] = useState("English");
-
-  const handleLanguageChange = (lang) => {
-    setLanguage(lang);
-    // Optionally, you can display content in the selected language.
-  };
-
   return (
     <div className="welcome-page">
       {/* Background video */}
@@ -17,12 +10,15 @@ const WelcomePage = () => {
         Your browser does not support the video tag.
       </video>
 
+      {/* Greetings in all three languages */}
+      <h1>ශ්‍රී ලංකා ග්‍රහලෝකාගාරය වෙත ඔබව සාදරයෙන් පිළිගනිමු!</h1>
+      <h1>இலங்கை கோளரங்கத்திற்கு வரவேற்கிறோம்!</h1>
       <h1>Welcome to the Sri Lanka Planetarium!</h1>
       
       <div className="language-selection">
-        <button onClick={() => handleLanguageChange("Sinhala")}>සිංහල</button>
-        <button onClick={() => handleLanguageChange("Tamil")}>தமிழ்</button>
-        <button onClick={() => handleLanguageChange("English")}>English</button>
+        <button>සිංහල</button>
+        <button>தமிழ்</button>
+        <button>English</button>
       </div>
     </div>
   );
